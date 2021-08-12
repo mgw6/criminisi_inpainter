c5e7a495aa9a7829f1e603dbcf7651cdcbe2ed11
MacGregor <magwin2012@gmail.com>
2021-08-11 23:38:28 -0400

    Added Crimisi subfolder

---
+++ criminisi/__init__.py
@@ -0,0 +1,26 @@
+#Since youre opening this I figure I may as well give you something to read. 
+
+"""
+(First lines; the screen shows a dark background as Lightning McQueen's voice is heard, inhales and exhales deeply)
+
+Lightning McQueen: Okay, here we go. Focus. Speed. I am speed.
+
+(Then, the screen shows some race cars whizzing past on a racetrack, then goes back to dark again)
+
+Lightning McQueen: One winner. 42 losers. I eat losers for breakfast.
+
+(Then, the screen shows McQueen from the back of his left side as his car accelerates and gains on a race car. Then, the screen goes dark again)
+
+Lightning McQueen: Breakfast? Wait, maybe I should have had breakfast? A little breaky could be good for me. No, no, no, stay focused. Speed!
+
+(Then, the screen shows race cars whizzing along the racetrack as the camera moves to the right. The screen then goes dark again)
+
+Lightning McQueen: I'm faster than fast, quicker than quick! I am Lightning!
+
+Mack: (pounds on his trailer door) Hey Lightning, you ready?
+
+Lightning McQueen: (Real Gone plays as the trailer door opens) Oh, yeah. Lightning's ready.
+
+
+Courtesy of: https://transcripts.fandom.com/wiki/Cars
+"""
